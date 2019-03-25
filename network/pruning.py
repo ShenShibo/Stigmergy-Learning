f6b87fce2da03ba1318899b20f90e890fe87b766
ShenShibo <490522301@qq.com>
2019-03-25 12:53:01 +0800

    small change

---
+++ network/pruning.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8-*-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from collections import OrderedDict
+import math
+from torch._jit_internal import weak_module
+from torch.nn.parameter import Parameter
+
+
+
+
+class PrunedNetwork(nn.Module):
+
+    def __init__(self):
+        super(PrunedNetwork, self).__init__()
+
+        
