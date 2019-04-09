b627629dd66b6fb993a0e4bbd9651d0242412b8a
ShenShibo <490522301@qq.com>
2019-04-09 11:10:13 +0800

    small change

--- main.py
+++ main.py
@@ -10,30 +10,7 @@ import argparse
 import torchvision.transforms as transforms
 from torchvision.datasets.cifar import CIFAR10 as dataset
 import copy
-
-
-def accuracy(outputs, labels):
-    _, predicted = torch.max(outputs.data, 1)
-    correct = (predicted == labels.data).sum()
-    return correct
-
-
-def validate(net, loader, use_cuda=False, device=0):
-    correct_count = 0.
-    count = 0.
-    if use_cuda:
-        net = net.cuda(device)
-    for i, (b_x, b_y) in enumerate(loader, 0):
-        size = b_x.shape[0]
-        if use_cuda:
-            b_x = b_x.cuda()
-            b_y = b_y.cuda()
-        outputs = net(b_x, i)
-        c = accuracy(outputs, b_y)
-        correct_count += c
-        count += size
-    acc = correct_count.item() / float(count)
-    return acc
+from process import *
 
 
 def train(args=None):
